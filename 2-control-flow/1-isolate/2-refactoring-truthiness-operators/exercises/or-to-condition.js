4a1473f05b0428c1e27d551012aec827213ea098
antonaksyuk <antonaksyuk@gmail.com>
2025-03-19 14:09:32 +0100

    add changes

--- 2-control-flow/1-isolate/2-refactoring-truthiness-operators/exercises/or-to-condition.js
+++ 2-control-flow/1-isolate/2-refactoring-truthiness-operators/exercises/or-to-condition.js
@@ -7,11 +7,11 @@ console.log('-- begin --');
 // refactor this code to use a condition instead of ||
 
 // try different values and different types
-const firstName = _;
-const lastName = _;
+const firstName = 'Anton';
+const lastName = 'Aksiuk';
 console.log(firstName, lastName);
 
-const hasAnAName = firstName[0] === 'A' || lastName[0] === 'A';
+const hasAnAName = firstName[0] === 'A' && lastName[0] === 'A';
 console.log(hasAnAName);
 
 console.log('-- end --');
